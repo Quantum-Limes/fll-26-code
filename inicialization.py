922244f8a3670cc909e4826987bd422da9059aa7
dany550 <dany.valek@seznam.cz>
2025-11-21 12:29:26 +0100

    inicialization update

--- inicialization.py
+++ inicialization.py
@@ -2,18 +2,19 @@ from spike_lib.arms import *
 from spike_lib.ultralocator import*
 
 hub = PrimeHub(front_side=Axis.Y)
-Lw = Motor(Port.B, Direction.COUNTERCLOCKWISE)
-Rw = Motor(Port.C, Direction.CLOCKWISE)
-Ra = Motor(Port.F, Direction.CLOCKWISE)
-La = Motor(Port.D, Direction.CLOCKWISE)
-Ul = UltrasonicSensor(Port.E)
-
 robot = Robot(hub)
+Lw = Motor(Port.B, Direction.COUNTERCLOCKWISE) #Lw = BetterMotor(robot, Port.B, Direction.COUNTERCLOCKWISE)
+Rw = Motor(Port.C, Direction.CLOCKWISE) #Rw = BetterMotor(robot, Port.C, Direction.CLOCKWISE)
+Ra = Motor(Port.F, Direction.CLOCKWISE) #Ra = BetterMotor(robot, Port.F, Direction.CLOCKWISE)
+La = Motor(Port.D, Direction.CLOCKWISE) #La = BetterMotor(robot, Port.D, Direction.CLOCKWISE)
+Ul = UltrasonicSensor(Port.E)
+Cs = ColorSensor(Port.A)
+
 drive = Drive(robot, left_motor=Lw, right_motor=Rw, wheel_diameter=56, axle_track=96)
 
-superruka = SuperArm(Ra, La, Color.YELLOW)
-#zvedaciruka = LiftArm(Ra, La, Color.RED)
-mlaticiruka = Mlaticka(Ra, La, Color.BLUE)
+superruka = SuperArm(robot, Ra, La, Color.YELLOW)
+#zvedaciruka = LiftArm(robot, Ra, La, Color.RED)
+mlaticiruka = Mlaticka(robot, Ra, La, Color.BLUE)
 
 ultra = UltraLocator(drive, Ul, 180, vec2(-25, 0))
 
